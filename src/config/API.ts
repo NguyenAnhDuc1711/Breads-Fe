98c0fed700a1cd67baf6c762f8e3dee7785f09c8
NguyenAnhDuc1711 <ducna17112003@gmail.com>
2026-08-28 14:51:43 +0700

    chore: update server port to 8080, adjust deployment path, and integrate CodeQL and Trivy security scanning

--- src/config/API.ts
+++ src/config/API.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import { API_PREFIX, Route, USER_PATH } from "../Breads-Shared/APIConfig";
 
 export const serverUrl: string =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 
 interface ApiOptions {
   path: string;
